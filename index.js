d44e641707054d840109792990e208cb0aca3713
Az-Heda <139802979+Az-Heda@users.noreply.github.com>
2023-09-08 16:39:14 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const all = require('./dist/server');
+
+module.exports = all;
+
+// console.clear();
+// const { WebServer, MySurreal } = all;
+// const server = new WebServer();
+// const sdb = new MySurreal({ username: 'root', password: 'root', namespace: 'test', database: 'test'});
